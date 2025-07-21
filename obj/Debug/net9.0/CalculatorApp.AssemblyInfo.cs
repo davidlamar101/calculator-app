e2606c7e1737b3488f6c59013147e67539dc342d
David La Mar <david.lamar101@gmail.com>
2025-07-21 12:00:52 -0500

    Initial commit - Simple Calculator App

--- obj/Debug/net9.0/CalculatorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ed70978bc388cc1328ee6a2dc025afc3017c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
